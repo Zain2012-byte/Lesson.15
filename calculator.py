bd5a3e3a880924c6d39cdebd27f710ab5526f5f3
Zain2012-byte <muzaffartawseefzain@gmail.com>
2024-11-24 18:13:20 +0530

    Lesson.15

---
+++ calculator.py
@@ -0,0 +1,35 @@
+def add(P,Q):
+    return P+Q
+
+def subtract(P,Q):
+    return P-Q
+
+def multiply(P,Q):
+    return P*Q
+
+def divide(P,Q):
+    return P/Q
+
+print("Plaese selct the operation:")
+
+print("a:-Add")
+print("B:- Subtract")
+print("C:- Multiply")
+print("D:- Divide" )
+
+choice=input("Please enter your choice a/b/c/d")
+
+num_1=int(input("Please enter the first number:"))
+num_2=int(input("Plese input the second number"))
+
+if choice=="a":
+    print(num_1,"+",num_2,"=", add(num_1,num_2))
+elif choice=="b":
+    print(num_1,"-",num_2,"=", subtract(num_1,num_2))
+elif choice=="c":
+    print(num_1,"*",num_2,"=", multiply(num_1,num_2))
+elif choice=="d":
+    print(num_1,"/",num_2,"=", divide(num_1,num_2))
+
+else:
+    print("This an invalid input")
